a5a1a2992648fb3c2d5ddfb9c73b1687f328804c
zhou1921336657 <191336657@qq.com>
2021-09-01 16:27:13 +0800

    ''

--- 京东拍卖/js/index_login.js
+++
@@ -1,7 +0,0 @@
-var oHello = document.getElementById("hello")
-var oSign = document.getElementById("sign")
-if(window.location.search != ""){
-	oSign.style.display = "none";
-	oHello.innerHTML = "你好，" + window.location.search.slice(1).split("=")[1];
-	oHello.href = "#";
-}
